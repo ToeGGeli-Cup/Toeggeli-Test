c5a782faf56bfd0f80c73bd9d89709142337375d
ToeGGeli-Cup <bzemann@gmail.com>
2025-03-10 16:17:10 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
-// Firebase SDK importieren
+// Import Firebase SDK-Module
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getDatabase, ref, push, set, get, onValue, remove, update } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+import { getDatabase, ref, push, set, remove, onValue } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
-// Firebase Konfiguration
+// Firebase-Konfiguration
 const firebaseConfig = {
     apiKey: "AIzaSyD2nmuFhdG0ZU5wO9H7CqsxPnRF04WZLzY",
     authDomain: "toeggeli-cup-2025.firebaseapp.com",
     databaseURL: "https://toeggeli-cup-2025-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "toeggeli-cup-2025",
-    storageBucket: "toeggeli-cup-2025.firebasestorage.app",
+    storageBucket: "toeggeli-cup-2025.appspot.com",
     messagingSenderId: "810841513587",
     appId: "1:810841513587:web:fc9d2b0274cf92b3ae1406"
 };
@@ -17,6 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Funktionen exportieren
-export { db, ref, push, set, get, onValue, remove, update };
-console.log("🔥 Firebase erfolgreich verbunden!");
+export { db, ref, push, set, remove, onValue };
